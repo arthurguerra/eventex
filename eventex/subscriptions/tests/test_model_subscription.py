8e63c92925488983ad81975696f5875804c2d372
ajguerra <arthurjguerra@gmail.com>
2016-08-10 23:04:51 -0300

    Implemented DB model for subscriptions

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from datetime import datetime
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Arthur Guerra',
+            cpf='12345678901',
+            email='arthurjguerra@gmail.com',
+            phone='73-98833-8187'
+        )
+
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django.contrib import messages
 from django.conf import settings
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -27,6 +28,8 @@ def create(request):
         'subscriptions/subscription_email.txt',
         form.cleaned_data
     )
+
+    Subscription.objects.create(**form.cleaned_data)
 
     # success feedback
     messages.success(request, 'Inscricao realizada com sucesso!')
