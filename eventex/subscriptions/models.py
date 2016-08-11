75a446416e21f9fea2deced71cf9fc87172757e6
ajguerra <arthurjguerra@gmail.com>
2016-08-10 23:47:31 -0300

    Created admin page (filter, verbose names - singular and plural, default ordering...)

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name='Name')
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
+    email = models.EmailField(verbose_name='Mail')
+    phone = models.CharField(max_length=20, verbose_name='Telephone #')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created Date')
+
+    class Meta:
+        verbose_name_plural = 'subscriptions_eventex'
+        verbose_name = 'subscription_eventex'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
