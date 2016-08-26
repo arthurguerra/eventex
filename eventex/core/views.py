75b15c580a48f31bb62cbeb135adac9ae5590124
ajguerra <arthurjguerra@gmail.com>
2016-08-25 22:56:22 -0300

    Implemented GenericHomeView

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -3,10 +3,28 @@ from django.views.generic import View
 from eventex.core.models import Speaker, Talk
 
 
-class HomeView(View):
+class GenericHomeView(View):
+    template_name = None
+    object_list = None
+    context_object_name = None
+
     def get(self, *args, **kwargs):
-        speakers = Speaker.objects.all()
-        return render(self.request, 'index.html', {'speakers': speakers})
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def render_to_response(self, context):
+        return render(self.request, self.template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = {self.context_object_name: self.object_list}
+        context.update(kwargs)
+        return context
+
+
+class HomeView(GenericHomeView):
+    template_name = 'index.html'
+    object_list = Speaker.objects.all()
+    context_object_name = 'speakers'
 
 
 def speaker_detail(request, slug):
