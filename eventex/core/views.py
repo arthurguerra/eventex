dafc635e54f73403ec5036b8b2d4db90feb8d2aa
ajguerra <arthurjguerra@gmail.com>
2016-08-25 23:17:24 -0300

    Implemented Talk list view as a ListView

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -6,15 +6,10 @@ from eventex.core.models import Speaker, Talk
 home = ListView.as_view(template_name='index.html',
                         model=Speaker)
 
+
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
 
 
-def talk_list(request):
-    context = {
-        'morning_talks': Talk.objects.at_morning(),
-        'afternoon_talks': Talk.objects.at_afternoon(),
-    }
-
-    return render(request, 'core/talk_list.html', context)
+talk_list = ListView.as_view(model=Talk)
